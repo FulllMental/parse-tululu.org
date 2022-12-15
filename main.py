05aa0a8e1d0ae7f3af167bdadb31e227b71489aa
FulllMental <104234625+FulllMental@users.noreply.github.com>
2022-12-15 23:44:13 +0300

    Download books with titles

--- main.py
+++ main.py
@@ -1,26 +1,46 @@
+import os
 from pathlib import Path
 
 import requests
+from bs4 import BeautifulSoup as BS
+from pathvalidate import sanitize_filename
 
 
-def get_book(url):
+def get_site_response(url):
     response = requests.get(url)
     response.raise_for_status()
     return response
 
 
-def download_book(site_response, filename, folder_name):
-    with open(f"{folder_name}/{filename}", 'wb') as file:
-        file.write(site_response.content)
+def check_for_redirect(response):
+    if response.url == "https://tululu.org/" or response.history:
+        raise requests.HTTPError()
+
+
+def download_txt(book_text, filename, folder='books/'):
+    Path(folder).mkdir(parents=True, exist_ok=True)
+    secure_path = os.path.join(folder, f'{sanitize_filename(filename)}.txt')
+    with open(secure_path, 'wb') as file:
+        file.write(book_text.content)
+
+
+def get_filename(book_index):
+    book_description_url = f"https://tululu.org/b{book_index}/"
+    frontpage_response = get_site_response(book_description_url)
+    soup = BS(frontpage_response.text, 'lxml')
+    title, author = soup.find('h1').text.split('::')
+    return f"{book_index}. {title.strip()}"
 
 
 if __name__ == '__main__':
-    folder_name = "books"
-    Path(folder_name).mkdir(parents=True, exist_ok=True)
+    for book_index in range(1, 11):
+        book_text_url = f"https://tululu.org/txt.php?id={book_index}"
+        book_text = get_site_response(book_text_url)
 
-    for book_index in range(10):
-        url = f"https://tululu.org/txt.php?id={book_index}"
-        filename = f'id{book_index + 1}.txt'
+        try:
+            check_for_redirect(book_text)
+        except requests.HTTPError:
+            continue
 
-        book = get_book(url)
-        download_book(book, filename, folder_name)
+        filename = get_filename(book_index)
+        download_txt(book_text, filename)
